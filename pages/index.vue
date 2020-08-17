1a67a446b626e15d1575b9b28b80f07f6b5ce5d9
aydeke <aydeke2@gmail.com>
2020-08-17 20:29:48 +0600

    Add head()

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
 <template lang="pug">
 h1 Hello Github pages!
 </template>
+<script>
+export default {
+  head() {
+    return {
+      meta: [
+        // Keywords
+        {
+          hid: `keywords`,
+          name: `keywords`,
+          content: `github pages play`,
+        },
+        // Description
+        {
+          hid: `description`,
+          name: `description`,
+          content: `My github pages play website`,
+        },
+      ],
+    }
+  },
+}
+</script>
